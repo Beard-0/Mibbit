77a2ffbf9a1ea63c2561784b0a53fd43b9758754
Beard-0 <Beard-0@outlook.com>
2014-08-29 16:54:22 +0800

    Initial commit

---
+++ app/src/main/java/com/teamtreehouse/mibbit/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.teamtreehouse.mibbit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+
+/**
+ * A login screen that offers login via email/password.
+
+ */
+public class LoginActivity extends Activity {
+
+
+    protected TextView mSignUpTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mSignUpTextView = (TextView) findViewById(R.id.signUpText);
+        mSignUpTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+
+
